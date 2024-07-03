d5ff395a5f88b6e4f0fdd22b00d9d49d828881dd
BaronClaps <126834072+BaronClaps@users.noreply.github.com>
2024-07-03 14:15:20 -0500

    Updated Follower Constants Specifically for the Pedro-Bot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/tuning/FollowerConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/tuning/FollowerConstants.java
@@ -22,14 +22,51 @@ public class FollowerConstants {
 
     // This section is for setting the actual drive vector for the front left wheel, if the robot
     // is facing a heading of 0 radians with the wheel centered at (0,0)
-    private static double xMovement = 70.0;
-    private static double yMovement = 50.0;
+    private static double xMovement = 50.81505075549097;
+    private static double yMovement = 60.542407306030555;
     private static double[] convertToPolar = Point.cartesianToPolar(xMovement, -yMovement);
     public static Vector frontLeftVector = MathFunctions.normalizeVector(new Vector(convertToPolar[0],convertToPolar[1]));
 
+    // Large translational PIDF coefficients
+    public static CustomPIDFCoefficients largeTranslationalPIDFCoefficients = new CustomPIDFCoefficients(
+            0.85,
+            0,
+            0,
+            0);
+
+    // Feed forward constant added on to the large translational PIDF
+    public static double largeTranslationalPIDFFeedForward = 0.015;
+
+    // Large translational Integral
+    public static CustomPIDFCoefficients largeTranslationalIntegral = new CustomPIDFCoefficients(
+            0,
+            0,
+            0,
+            0);
+
+    // the limit at which the heading PIDF switches between the large and small translational PIDFs
+    public static double translationalPIDFSwitch = 3;
+
+    // Small translational PIDF coefficients
+    public static CustomPIDFCoefficients smallTranslationalPIDFCoefficients = new CustomPIDFCoefficients(
+            0.215,
+            0,
+            0.01,
+            0);
+
+    // Small translational Integral value
+    public static CustomPIDFCoefficients smallTranslationalIntegral = new CustomPIDFCoefficients(
+            0,
+            0,
+            0,
+            0);
+
+    // Feed forward constant added on to the small translational PIDF
+    public static double smallTranslationalPIDFFeedForward = 0.015;
+
     // Large heading error PIDF coefficients
     public static CustomPIDFCoefficients largeHeadingPIDFCoefficients = new CustomPIDFCoefficients(
-            1.5,
+            1,
             0,
             0,
             0);
@@ -42,7 +79,7 @@ public class FollowerConstants {
 
     // Small heading error PIDF coefficients
     public static CustomPIDFCoefficients smallHeadingPIDFCoefficients = new CustomPIDFCoefficients(
-            3.25,
+            2.1,
             0,
             0.08,
             0);
@@ -50,84 +87,42 @@ public class FollowerConstants {
     // Feed forward constant added on to the small heading PIDF
     public static double smallHeadingPIDFFeedForward = 0.01;
 
-    // Large translational PIDF coefficients
-    public static CustomPIDFCoefficients largeTranslationalPIDFCoefficients = new CustomPIDFCoefficients(
-            0.15,
-            0,
-            0.005,
-            0);
-
-    // Feed forward constant added on to the large translational PIDF
-    public static double largeTranslationalPIDFFeedForward = 0.01;
-
-    // Large translational Integral
-    public static CustomPIDFCoefficients largeTranslationalIntegral = new CustomPIDFCoefficients(
-            0,
-            0,
-            0,
-            0);
-
-    // the limit at which the strafe PIDF switches between the large and small translational PIDFs
-    public static double translationalPIDFSwitch = 3;
-
-    // Small translational PIDF coefficients
-    public static CustomPIDFCoefficients smallTranslationalPIDFCoefficients = new CustomPIDFCoefficients(
-            0.325,
-            0,
-            0.01,
-            0);
-
-    // Small translational Integral value
-    public static CustomPIDFCoefficients smallTranslationalIntegral = new CustomPIDFCoefficients(
-            0,
-            0,
-            0,
-            0);
-
-    
-    // Feed forward constant added on to the small translational PIDF
-    public static double smallTranslationalPIDFFeedForward = 0.015;
-
     // Large drive PIDF coefficients
     public static CustomPIDFCoefficients largeDrivePIDFCoefficients = new CustomPIDFCoefficients(
-            0.015,
+            0.0075,
             0,
-            0.0005,
+            0.000001,
             0);
 
     // Feed forward constant added on to the large drive PIDF
     public static double largeDrivePIDFFeedForward = 0.01;
 
-    // the limit at which the direction PIDF switches between the large and small drive PIDFs
-    public static double drivePIDFSwitch = 10;
+    // the limit at which the heading PIDF switches between the large and small drive PIDFs
+    public static double drivePIDFSwitch = 20;
 
     // Small drive PIDF coefficients
     public static CustomPIDFCoefficients smallDrivePIDFCoefficients = new CustomPIDFCoefficients(
-            0.0225,
+            0.007,
             0,
-            0.0005,
+            0.000005,
             0);
 
     // Feed forward constant added on to the small drive PIDF
     public static double smallDrivePIDFFeedForward = 0.01;
 
     // Mass of robot in kilograms
-    public static double mass = 14.5; //10.65942;
+    public static double mass = 6.577089;
 
     // Centripetal force to power scaling
-    // todo: there are currently issues with the centripetal force correction, so just don't use it for now
-    // i will fix these in another commit soon
-    public static double centripetalScaling = 0.0002;
+    public static double centripetalScaling = 0.0006;
 
     // Acceleration of the drivetrain when power is cut in inches/second^2 (should be negative)
     // if not negative, then the robot thinks that its going to go faster under 0 power
-    // this is for curves
-    public static double forwardZeroPowerAcceleration = -30.0551;;//-30.4551;
+    public static double forwardZeroPowerAcceleration = -31.877142560304716;
 
     // Acceleration of the drivetrain when power is cut in inches/second^2 (should be negative)
     // if not negative, then the robot thinks that its going to go faster under 0 power
-    // this is for curves
-    public static double lateralZeroPowerAcceleration = -31.51952;//-91.51952;
+    public static double lateralZeroPowerAcceleration = -52.5078462290507;
 
     // A multiplier for the zero power acceleration to change the speed the robot decelerates at
     // the end of paths.
@@ -136,32 +131,32 @@ public class FollowerConstants {
     // Decreasing this will cause the deceleration at the end of the Path to be slower, making the
     // robot slower but reducing risk of end-of-path overshoots or localization slippage.
     // This can be set individually for each Path, but this is the default.
-    public static double zeroPowerAccelerationMultiplier = 4;
+    public static double zeroPowerAccelerationMultiplier = 2.45;
 
     // When the robot is at the end of its current Path or PathChain and the velocity goes below
     // this value, then end the Path. This is in inches/second.
     // This can be custom set for each Path.
-    public static double pathEndVelocityConstraint = 5.0;
+    public static double pathEndVelocityConstraint = 0.1;
 
     // When the robot is at the end of its current Path or PathChain and the translational error
     // goes below this value, then end the Path. This is in inches.
     // This can be custom set for each Path.
-    public static double pathEndTranslationalConstraint = 0.7;
+    public static double pathEndTranslationalConstraint = 0.1;
 
     // When the robot is at the end of its current Path or PathChain and the heading error goes
     // below this value, then end the Path. This is in radians.
     // This can be custom set for each Path.
-    public static double pathEndHeadingConstraint = 0.03;
+    public static double pathEndHeadingConstraint = 0.007;
 
     // When the t-value of the closest point to the robot on the Path is greater than this value,
     // then the Path is considered at its end.
     // This can be custom set for each Path.
-    public static double pathEndTValueConstraint = 0.990;
+    public static double pathEndTValueConstraint = 0.995;
 
     // When the Path is considered at its end parametrically, then the Follower has this many
-    // seconds to further correct by default.
+    // milliseconds to further correct by default.
     // This can be custom set for each Path.
-    public static double pathEndTimeoutConstraint = 100.0;
+    public static double pathEndTimeoutConstraint = 500;
 
     // This is how many steps the BezierCurve class uses to approximate the length of a BezierCurve.
     public static int APPROXIMATION_STEPS = 1000;
@@ -175,7 +170,7 @@ public class FollowerConstants {
     // This is the number of times the velocity is recorded for averaging when approximating a first
     // and second derivative for on the fly centripetal correction. The velocity is calculated using
     // half of this number of samples, and the acceleration uses all of this number of samples.
-    public static int AVERAGED_VELOCITY_SAMPLE_NUMBER = 208;
+    public static int AVERAGED_VELOCITY_SAMPLE_NUMBER = 8;
 
     // This is the number of steps the binary search for closest point uses. More steps is more
     // accuracy, and this increases at an exponential rate. However, more steps also does take more
